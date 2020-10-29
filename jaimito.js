f5917c227d4756e8a50bb136b385c6482dc34553
jaimitog7 <jaime.garcia1@virginiogomez.cl>
2020-10-28 23:46:10 -0300

    commit en nuevarama

--- jaimito.js
+++ jaimito.js
@@ -3,3 +3,19 @@
         alert('paso por aqui')
     }
 </script>
+
+
+
+…or create a new repository on the command line
+echo "# pruebagit" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jaimitog7/pruebagit.git
+git push -u origin main
+                
+…or push an existing repository from the command line
+git remote add origin https://github.com/jaimitog7/pruebagit.git
+git branch -M main
+git push -u origin main
